0e65d772d1cbbee5bf5b237710ab3a684afcb208
Redae Beraki <redae.beraki@gmail.com>
2024-06-20 22:01:00 -0500

    Replace endpoint anonymous functions with private methods

--- UsersJwtAuthApp/UsersJwtAuth/Endpoints/UserEndpoints.cs
+++ UsersJwtAuthApp/UsersJwtAuth/Endpoints/UserEndpoints.cs
@@ -8,59 +8,69 @@ public static class UserEndpoints
 {
     public static void AddUserEndpoints(this WebApplication app)
     {
-        app.MapGet("/users", async (IUserRepository repo) =>
+        app.MapGet("/users", GetUsers);
+
+        app.MapPost("users/register", CreateUser);
+
+        app.MapPost("/users/login", GetToken);
+
+        app.MapPut("/users/{id}", UpdateUser);
+
+        app.MapDelete("/users/{id}", DeleteUser);
+    }
+
+    private async static Task<IResult> GetUsers(IUserRepository repo)
+    {
+        var result = await repo.GetAll();
+        return Results.Ok(result);
+    }
+
+    private async static Task<IResult> CreateUser(IUserRepository repo, [FromBody] UserDto userDto)
+    {
+        var user = new User
         {
-            var result = await repo.GetAll();
-            return Results.Ok(result);
-        });
+            Username = userDto.Username,
+            Password = userDto.Password, // TODO: Hash password
+            Email = userDto.Email,
+            FirstName = userDto.Firstname,
+            LastName = userDto.Lastname
+        };
 
-        app.MapPost("users/register", async (IUserRepository repo, [FromBody] UserDto userDto) =>
-        {
-            var user = new User
-            {
-                Username = userDto.Username,
-                Password = userDto.Password, // TODO: Hash password
-                Email = userDto.Email,
-                FirstName = userDto.Firstname,
-                LastName = userDto.Lastname
-            };
+        await repo.Add(user);
 
-            await repo.Add(user);
+        return Results.Ok();
+    }
 
-            return Results.Ok();
-        });
+    private async static Task<IResult> GetToken(IUserRepository repo, IConfiguration configuration, [FromBody] UserLoginDto userDto)
+    {
+        var user = await repo.GetByUsername(userDto.Username);
 
-        app.MapPost("/users/login", async (IUserRepository repo, IConfiguration configuration, [FromBody] UserLoginDto userDto) =>
-        {
-            var user = await repo.GetByUsername(userDto.Username);
+        // TODO: login and get access token
 
-            // TODO: login and get access token
+        return Results.Ok(new TokenDto("", ""));
+    }
 
-            return Results.Ok(new TokenDto("", ""));
-        });
+    private async static Task<IResult> UpdateUser(IUserRepository repo, int id, [FromBody] UserDto userDto)
+    {
+        var user = await repo.GetById(id);
 
-        app.MapPut("/users/{id}", async (IUserRepository repo, int id, [FromBody] UserDto userDto) =>
-        {
-            var user = await repo.GetById(id);
+        if (user == null)
+            return Results.NotFound("User not found");
 
-            if (user == null)
-                return Results.NotFound("User not found");
+        user.FirstName = userDto.Firstname;
+        user.LastName = userDto.Lastname;
+        user.Email = userDto.Email;
+        user.Password = userDto.Password; // TODO: Hash password
 
-            user.FirstName = userDto.Firstname;
-            user.LastName = userDto.Lastname;
-            user.Email = userDto.Email;
-            user.Password = userDto.Password; // TODO: Hash password
+        await repo.Update(user);
 
-            await repo.Update(user);
+        return Results.Ok();
+    }
 
-            return Results.Ok();
-        });
+    private async static Task<IResult> DeleteUser(IUserRepository repo, int id)
+    {
+        await repo.Delete(id);
 
-        app.MapDelete("/users/{id}", async (IUserRepository repo, int id) =>
-        {
-            await repo.Delete(id);
-
-            return Results.Ok();
-        });
+        return Results.Ok();
     }
 }
