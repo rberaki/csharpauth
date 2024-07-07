190f52a61e1dfdac3d6d808094abc7ce3d26ac0a
Redae Beraki <redae.beraki@gmail.com>
2024-07-07 15:23:36 -0500

    Move auth dependency outta program dot cs

--- UsersJwtAuthApp/UsersJwtAuth/Program.cs
+++ UsersJwtAuthApp/UsersJwtAuth/Program.cs
@@ -1,9 +1,7 @@
-using System.Text;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using UsersJwtAuth.Endpoints;
 using UsersJwtAuth.Repositories;
+using UsersJwtAuth.StartupDependency;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,27 +10,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration.GetValue<string>("Auth:Issuer"),
-            ValidAudience = builder.Configuration.GetValue<string>("Auth:Audience"),
-            IssuerSigningKey =
-                new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Auth:SecretKey")))
-        };
-    });
-
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-});
+builder.Services.AddAuthServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ UsersJwtAuthApp/UsersJwtAuth/StartupDependency/DIExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
+namespace UsersJwtAuth.StartupDependency;
+
+public static class DiExtensions
+{
+    public static void AddAuthServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        });
+
+        services.AddAuthentication("Bearer").AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config.GetValue<string>("Auth:Issuer"),
+                ValidAudience = config.GetValue<string>("Auth:Audience"),
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(config.GetValue<string>("Auth:SecretKey")))
+            };
+        });
+    }
+}
